f8fdfa85f0ddf8a957475d103149df08731bda76
Franco-611 <69815571+Franco-611@users.noreply.github.com>
2023-04-24 13:58:34 -0600

    Co-authored-by: JuanDiegoAvila <JuanDiegoAvila@users.noreply.github.com> Co-authored-by: Manuel-Archila <Manuel-Archila@users.noreply.github.com>

--- string_generator.py
+++ string_generator.py
@@ -1,12 +1,12 @@
 import random
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-strings_num = 20
+numbers   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] 
+strings_num = 10
 strings_generadas = []
 for i in range(strings_num):
     strings = []
-    length = (i+1) * 10
     for i in range(2):
+        length = random.choice(numbers)
         string = ""
         for j in range(length):
             string += random.choice(letters)
